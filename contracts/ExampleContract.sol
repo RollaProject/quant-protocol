f4a1c233228c7d40dd2994848485cd409488e25b
quant-spiz <78666323+quant-spiz@users.noreply.github.com>
2021-02-08 16:41:15 +0000

    Files for docs

--- contracts/ExampleContract.sol
+++ contracts/ExampleContract.sol
@@ -9,7 +9,11 @@ contract ExampleContract {
     /// @dev Function dev
     /// @param paramName function param
     /// @return returnVal function return val
-    function exampleFunction(uint256 paramName) external pure returns (uint256) {
+    function exampleFunction(uint256 paramName)
+        external
+        pure
+        returns (uint256)
+    {
         return paramName + 1;
     }
 }
