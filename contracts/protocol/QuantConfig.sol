aab84fd6292b36842295c6eab4dff6b08a1a946d
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-02-11 22:36:02 -0300

    Use OpenZeppelin's upgrade plugins to make QuantConfig upgradable through a proxy
    Close #7
    Also update docs

--- contracts/protocol/QuantConfig.sol
+++ contracts/protocol/QuantConfig.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 
 /// @title A central config for the quant system. Also acts as a central access control manager.
 /// @notice For storing constants, variables and allowing them to be changed by the admin (governance)
 /// @dev This should be used as a central access control manager which other contracts use to check permissions
-contract QuantConfig is AccessControl {
+contract QuantConfig is AccessControl, Initializable {
     //this should be some admin/governance address
     address public admin;
     uint256 public fee;
@@ -14,20 +15,20 @@ contract QuantConfig is AccessControl {
     bytes32 public constant OPTIONS_CONTROLLER_ROLE =
         keccak256("OPTIONS_CONTROLLER_ROLE");
 
-    /// @notice Initializes the system roles and assign them to the given admin address
-    /// @param _admin Address to receive the system roles
-    constructor(address _admin) {
-        _setupRole(DEFAULT_ADMIN_ROLE, _admin);
-        // On deployment, this role should be transferd to the OptionsFactory as its only admin
-        _setupRole(OPTIONS_CONTROLLER_ROLE, _admin);
-        admin = _admin;
-    }
-
     /// @notice Set the protocol fee
     /// @dev Only accounts or contracts with the admin role should call this function
     /// @param _fee The new amount to set as the protocol fee
     function setFee(uint256 _fee) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not admin");
         fee = _fee;
+    }
+
+    /// @notice Initializes the system roles and assign them to the given admin address
+    /// @param _admin Address to receive the system roles
+    function initialize(address _admin) public initializer {
+        _setupRole(DEFAULT_ADMIN_ROLE, _admin);
+        // On deployment, this role should be transferd to the OptionsFactory as its only admin
+        _setupRole(OPTIONS_CONTROLLER_ROLE, _admin);
+        admin = _admin;
     }
 }
---
+++ docs/protocol/options/OptionsFactory.md
@@ -0,0 +1,60 @@
+## `OptionsFactory`
+
+
+
+
+
+
+### `constructor(address quantConfig_)` (public)
+
+Initializes a new options factory
+
+
+
+
+### `createOption(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → address` (external)
+
+Creates new QTokens
+
+
+The CREATE2 opcode is used to deterministically deploy new QTokens
+
+
+### `getTargetQTokenAddress(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → address` (external)
+
+get the address at which a new QToken with the given parameters would be deployed
+return the exact address the QToken will be deployed at with OpenZeppelin's Create2
+library computeAddress function
+
+
+
+
+### `getQToken(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → address` (external)
+
+get the QToken address for an already created QToken, if no QToken has been created
+with these parameters, it will return the zero address
+
+
+
+
+### `getOptionsLength() → uint256` (external)
+
+get the total number of options created by the factory
+
+
+
+
+### `_optionHash(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → bytes32` (internal)
+
+Returns a unique option hash based on its parameters
+
+
+
+
+
+### `OptionCreated(address optionTokenAddress, address creator, address underlying, address strike, address oracle, uint256 strikePrice, uint256 expiry, bool isCall)`
+
+emitted when the factory creates a new option
+
+
+
