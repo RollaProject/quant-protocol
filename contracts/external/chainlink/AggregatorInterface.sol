16e44bad445ae81cf7414d08b620668d8257c43c
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-02-16 17:52:51 -0300

    Make solhint and tests pass

--- contracts/external/chainlink/AggregatorInterface.sol
+++ contracts/external/chainlink/AggregatorInterface.sol
@@ -2,16 +2,6 @@
 pragma solidity ^0.7.0;
 
 interface AggregatorInterface {
-    function latestAnswer() external view returns (int256);
-
-    function latestTimestamp() external view returns (uint256);
-
-    function latestRound() external view returns (uint256);
-
-    function getAnswer(uint256 roundId) external view returns (int256);
-
-    function getTimestamp(uint256 roundId) external view returns (uint256);
-
     event AnswerUpdated(
         int256 indexed current,
         uint256 indexed roundId,
@@ -22,4 +12,14 @@ interface AggregatorInterface {
         address indexed startedBy,
         uint256 startedAt
     );
+
+    function latestAnswer() external view returns (int256);
+
+    function latestTimestamp() external view returns (uint256);
+
+    function latestRound() external view returns (uint256);
+
+    function getAnswer(uint256 roundId) external view returns (int256);
+
+    function getTimestamp(uint256 roundId) external view returns (uint256);
 }
