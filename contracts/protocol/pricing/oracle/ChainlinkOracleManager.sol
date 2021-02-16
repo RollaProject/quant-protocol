74081c56d33691f7ead58d14f128c3e0344c59b0
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-02-16 17:52:51 -0300

    Make solhint and tests pass

--- contracts/protocol/pricing/oracle/ChainlinkOracleManager.sol
+++ contracts/protocol/pricing/oracle/ChainlinkOracleManager.sol
@@ -8,31 +8,17 @@ import "./ProviderOracleManager.sol";
 /// @title For managing chainlink oracles and which assets use them
 /// @notice Once an oracle is added for an asset it can't be changed!
 contract ChainlinkOracleManager is ProviderOracleManager {
+    event ChainlinkPriceSubmission(
+        address asset,
+        uint256 expiryTimestamp,
+        uint256 price,
+        uint256 roundId,
+        address oracle
+    );
+
     /// @param _config address of quant central configuration
+    // solhint-disable-next-line no-empty-blocks
     constructor(address _config) ProviderOracleManager(_config) {}
-
-    /// @notice Get the expiry price from oracle and store it in the price registry so we have a copy
-    /// @param _asset asset to get price of
-    function getCurrentPrice(address _asset)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        require(
-            assetOracles[_asset] != address(0),
-            "ChainlinkOracleManager: Asset not supported"
-        );
-        AggregatorInterface aggregator =
-            AggregatorInterface(assetOracles[_asset]);
-        int256 answer = aggregator.latestAnswer();
-        require(
-            answer > 0,
-            "ChainlinkOracleManager: No pricing data available"
-        );
-
-        return uint256(answer);
-    }
 
     /// @notice Get the expiry price from oracle and store it in the price registry so we have a copy
     /// @param _asset asset to set price of
@@ -73,11 +59,26 @@ contract ChainlinkOracleManager is ProviderOracleManager {
         );
     }
 
-    event ChainlinkPriceSubmission(
-        address asset,
-        uint256 expiryTimestamp,
-        uint256 price,
-        uint256 roundId,
-        address oracle
-    );
+    /// @notice Get the expiry price from oracle and store it in the price registry so we have a copy
+    /// @param _asset asset to get price of
+    function getCurrentPrice(address _asset)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        require(
+            assetOracles[_asset] != address(0),
+            "ChainlinkOracleManager: Asset not supported"
+        );
+        AggregatorInterface aggregator =
+            AggregatorInterface(assetOracles[_asset]);
+        int256 answer = aggregator.latestAnswer();
+        require(
+            answer > 0,
+            "ChainlinkOracleManager: No pricing data available"
+        );
+
+        return uint256(answer);
+    }
 }
