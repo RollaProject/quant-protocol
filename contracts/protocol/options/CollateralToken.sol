8295bb4726729532fe09ff60c30a08adb1fbf5e7
Options Eskimo <78507171+optionseskimo@users.noreply.github.com>
2021-02-19 01:35:37 +0000

    feature/options contracts (#11)
    * Add initial CollateralToken code
    
    * Asset tokens could have different decimals
    
    for example, USDC has 6, wBTC has 8
    
    * Add docs and store token ids instead of hashes
    
    * Normalize NatSpec docs and variable naming
    
    Normalize NatSpec docs throughout the project to use "///" and
    follow the convention of starting parameters of internal functions
    with "_"
    
    * Restrict access to CollateralToken creation
    
    Also add more documentation
    
    * Implement batched minting and burning of tokens
    
    * Add contract description as NatSpec comments
    
    * Update OptionsFactory to create CollateralTokens alongside QTokens
    
    * Add description to the OptionsFactory contract
    
    * Make notice more clear
    
    * Remove duplicate data stored on CollateralToken
    
    * Abstract the logic to get a token id and restrict creation
    
    * Keep track of the tokenSupply of each individual token

---
+++ contracts/protocol/options/CollateralToken.sol
@@ -0,0 +1,185 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "../QuantConfig.sol";
+
+/// @title Tokens representing a Quant user's short positions
+/// @author Quant Finance
+/// @notice Can be used by owners to claim their collateral
+/// @dev This is a multi-token contract that implements the ERC1155 token standard:
+/// https://eips.ethereum.org/EIPS/eip-1155
+contract CollateralToken is ERC1155 {
+    using SafeMath for uint256;
+
+    /// @dev stores metadata for a CollateralToken with an specific id
+    /// @param qTokenAddress address of the corresponding QToken
+    /// @param collateralizedFrom initial spread collateral
+    struct CollateralTokenInfo {
+        address qTokenAddress;
+        uint256 collateralizedFrom;
+    }
+
+    /// @dev The Quant system config
+    QuantConfig public quantConfig;
+
+    /// @dev mapping of CollateralToken ids to their respective info struct
+    mapping(uint256 => CollateralTokenInfo) private _idToInfo;
+
+    /// @notice array of all the created CollateralToken ids
+    uint256[] public collateralTokensIds;
+
+    /// @notice mapping from token ids to their supplies
+    mapping(uint256 => uint256) public tokenSupplies;
+
+    /// @notice Initializes a new ERC1155 multi-token contract for representing
+    /// users' short positions
+    /// @param _quantConfig the address of the Quant system configuration contract
+    constructor(address _quantConfig) ERC1155("URI") {
+        quantConfig = QuantConfig(_quantConfig);
+    }
+
+    /// @notice Create new CollateralTokens
+    /// @param _qTokenAddress address of the corresponding QToken
+    /// @param _collateralizedFrom initial spread collateral
+    /// @return id the id for the CollateralToken created with the given arguments
+    function createCollateralToken(
+        address _qTokenAddress,
+        uint256 _collateralizedFrom
+    ) external returns (uint256 id) {
+        id = _collateralTokenId(_qTokenAddress, _collateralizedFrom);
+
+        require(
+            quantConfig.hasRole(
+                quantConfig.OPTIONS_CONTROLLER_ROLE(),
+                msg.sender
+            ),
+            "CollateralToken: Only the OptionsFactory can create new CollateralTokens"
+        );
+
+        require(
+            _idToInfo[id].qTokenAddress == address(0),
+            "CollateralToken: this token has already been created"
+        );
+
+        _idToInfo[id] = CollateralTokenInfo({
+            qTokenAddress: _qTokenAddress,
+            collateralizedFrom: _collateralizedFrom
+        });
+
+        collateralTokensIds.push(id);
+    }
+
+    /// @notice Mint CollateralTokens for a given account
+    /// @param recipient address to receive the minted tokens
+    /// @param amount amount of tokens to mint
+    /// @param collateralTokenId id of the token to be minted
+    function mintCollateralToken(
+        address recipient,
+        uint256 amount,
+        uint256 collateralTokenId
+    ) external {
+        require(
+            quantConfig.hasRole(
+                quantConfig.OPTIONS_CONTROLLER_ROLE(),
+                msg.sender
+            ),
+            "CollateralToken: Only the OptionsFactory can mint CollateralTokens"
+        );
+        _mint(recipient, amount, collateralTokenId, "");
+
+        tokenSupplies[collateralTokenId] = tokenSupplies[collateralTokenId].add(
+            amount
+        );
+    }
+
+    /// @notice Mint CollateralTokens for a given account
+    /// @param owner address to burn tokens from
+    /// @param amount amount of tokens to burn
+    /// @param collateralTokenId id of the token to be burned
+    function burnCollateralToken(
+        address owner,
+        uint256 amount,
+        uint256 collateralTokenId
+    ) external {
+        require(
+            quantConfig.hasRole(
+                quantConfig.OPTIONS_CONTROLLER_ROLE(),
+                msg.sender
+            ),
+            "CollateralToken: Only the OptionsFactory can burn CollateralTokens"
+        );
+        _burn(owner, collateralTokenId, amount);
+
+        tokenSupplies[collateralTokenId] = tokenSupplies[collateralTokenId].sub(
+            amount
+        );
+    }
+
+    /// @notice Batched minting of multiple CollateralTokens for a given account
+    /// @dev Should be used when minting multiple CollateralTokens for a single user,
+    /// i.e., when a user buys more than one short position through the interface
+    /// @param recipient address to receive the minted tokens
+    /// @param ids array of CollateralToken ids to be minted
+    /// @param amounts array of amounts of tokens to be minted
+    /// @dev ids and amounts must have the same length
+    function mintCollateralTokenBatch(
+        address recipient,
+        uint256[] calldata ids,
+        uint256[] calldata amounts
+    ) external {
+        require(
+            quantConfig.hasRole(
+                quantConfig.OPTIONS_CONTROLLER_ROLE(),
+                msg.sender
+            ),
+            "CollateralToken: Only the OptionsFactory can mint CollateralTokens"
+        );
+        _mintBatch(recipient, ids, amounts, "");
+
+        for (uint256 i = 0; i < ids.length; i = i.add(i)) {
+            tokenSupplies[ids[i]] = tokenSupplies[ids[i]].add(amounts[i]);
+        }
+    }
+
+    /// @notice Batched burning of of multiple CollateralTokens from a given account
+    /// @dev Should be used when burning multiple CollateralTokens for a single user,
+    /// i.e., when a user sells more than one short position through the interface
+    /// @param owner address to burn tokens from
+    /// @param ids array of CollateralToken ids to be burned
+    /// @param amounts array of amounts of tokens to be burned
+    /// @dev ids and amounts shoud have the same length
+    function burnCollateralTokenBatch(
+        address owner,
+        uint256[] calldata ids,
+        uint256[] calldata amounts
+    ) external {
+        require(
+            quantConfig.hasRole(
+                quantConfig.OPTIONS_CONTROLLER_ROLE(),
+                msg.sender
+            ),
+            "CollateralToken: Only the OptionsFactory can burn CollateralTokens"
+        );
+        _burnBatch(owner, ids, amounts);
+
+        for (uint256 i = 0; i < ids.length; i = i.add(i)) {
+            tokenSupplies[ids[i]] = tokenSupplies[ids[i]].sub(amounts[i]);
+        }
+    }
+
+    /// @dev Returns a unique CollateralToken id based on its parameters
+    /// @param _qToken the address of the corresponding QToken
+    /// @param _collateralizedFrom initial spread collateral
+    /// @return id the id for the CollateralToken with the given arguments
+    function _collateralTokenId(address _qToken, uint256 _collateralizedFrom)
+        internal
+        pure
+        returns (uint256 id)
+    {
+        id = uint256(keccak256(abi.encodePacked(_qToken, _collateralizedFrom)));
+    }
+}
--- docs/protocol/options/OptionsFactory.md
+++ docs/protocol/options/OptionsFactory.md
@@ -1,14 +1,25 @@
 ## `OptionsFactory`
 
-### `constructor(address quantConfig_)` (public)
+Creates tokens for long (QToken) and short (CollateralToken) positions
+
+
+This contract follows the factory design pattern
+
+
+### `constructor(address quantConfig_, address collateralToken_)` (public)
 
 Initializes a new options factory
 
-### `createOption(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → address` (external)
 
-Creates new QTokens
+
+
+### `createOption(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → address newQToken, uint256 newCollateralTokenId` (external)
+
+Creates new options (QToken + CollateralToken)
+
 
 The CREATE2 opcode is used to deterministically deploy new QTokens
+
 
 ### `getTargetQTokenAddress(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → address` (external)
 
@@ -16,19 +27,35 @@ get the address at which a new QToken with the given parameters would be deploye
 return the exact address the QToken will be deployed at with OpenZeppelin's Create2
 library computeAddress function
 
+
+
+
 ### `getQToken(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → address` (external)
 
 get the QToken address for an already created QToken, if no QToken has been created
 with these parameters, it will return the zero address
 
+
+
+
 ### `getOptionsLength() → uint256` (external)
 
 get the total number of options created by the factory
 
-### `_optionHash(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → bytes32` (internal)
+
+
+
+### `_qTokenHash(address _underlyingAsset, address _strikeAsset, address _oracle, uint256 _strikePrice, uint256 _expiryTime, bool _isCall) → bytes32` (internal)
 
 Returns a unique option hash based on its parameters
 
-### `OptionCreated(address optionTokenAddress, address creator, address underlying, address strike, address oracle, uint256 strikePrice, uint256 expiry, bool isCall)`
+
+
+
+
+### `OptionCreated(address qTokenAddress, address creator, address underlying, address strike, address oracle, uint256 strikePrice, uint256 expiry, uint256 collateralTokenId, bool isCall)`
 
 emitted when the factory creates a new option
+
+
+
