ee82add867a99039ff9de44c5eef2b79c2f5b7dd
Options Eskimo <78507171+optionseskimo@users.noreply.github.com>
2021-02-19 01:35:37 +0000

    feature/options contracts (#11)
    * Add initial CollateralToken code
    
    * Asset tokens could have different decimals
    
    for example, USDC has 6, wBTC has 8
    
    * Add docs and store token ids instead of hashes
    
    * Normalize NatSpec docs and variable naming
    
    Normalize NatSpec docs throughout the project to use "///" and
    follow the convention of starting parameters of internal functions
    with "_"
    
    * Restrict access to CollateralToken creation
    
    Also add more documentation
    
    * Implement batched minting and burning of tokens
    
    * Add contract description as NatSpec comments
    
    * Update OptionsFactory to create CollateralTokens alongside QTokens
    
    * Add description to the OptionsFactory contract
    
    * Make notice more clear
    
    * Remove duplicate data stored on CollateralToken
    
    * Abstract the logic to get a token id and restrict creation
    
    * Keep track of the tokenSupply of each individual token

--- contracts/protocol/options/OptionsFactory.sol
+++ contracts/protocol/options/OptionsFactory.sol
@@ -4,48 +4,59 @@ pragma solidity ^0.7.0;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Create2.sol";
 import "./QToken.sol";
+import "./CollateralToken.sol";
 import "../QuantConfig.sol";
 
+/// @title Factory contract for Quant options
+/// @author Quant Finance
+/// @notice Creates tokens for long (QToken) and short (CollateralToken) positions
+/// @dev This contract follows the factory design pattern
 contract OptionsFactory {
     using SafeMath for uint256;
 
-    /// @notice array of all the created options
-    address[] public options;
+    /// @notice array of all the created QTokens
+    address[] public qTokens;
 
     QuantConfig private _quantConfig;
+
+    CollateralToken private _collateralToken;
 
     /// @dev constant salt because options will only be deployed with the same parameters once
     bytes32 private constant _SALT = bytes32(0);
 
-    mapping(bytes32 => address) private _hashToAddress;
+    mapping(bytes32 => address) private _qTokenHashToAddress;
 
     /// @notice emitted when the factory creates a new option
     event OptionCreated(
-        address optionTokenAddress,
+        address qTokenAddress,
         address creator,
         address indexed underlying,
         address indexed strike,
         address oracle,
         uint256 strikePrice,
         uint256 expiry,
+        uint256 collateralTokenId,
         bool isCall
     );
 
     /// @notice Initializes a new options factory
     /// @param quantConfig_ the address of the Quant system configuration contract
-    constructor(address quantConfig_) {
+    /// @param collateralToken_ address of the CollateralToken contract
+    constructor(address quantConfig_, address collateralToken_) {
         _quantConfig = QuantConfig(quantConfig_);
+        _collateralToken = CollateralToken(collateralToken_);
     }
 
-    /// @notice Creates new QTokens
+    /// @notice Creates new options (QToken + CollateralToken)
     /// @dev The CREATE2 opcode is used to deterministically deploy new QTokens
     /// @param _underlyingAsset asset that the option references
     /// @param _strikeAsset asset that the strike is denominated in
     /// @param _oracle price oracle for the option underlying
-    /// @param _strikePrice strike price with 18 decimals
+    /// @param _strikePrice strike price with as many decimals in the strike asset
     /// @param _expiryTime expiration timestamp as a unix timestamp
     /// @param _isCall true if it's a call option, false if it's a put option
-    /// @return address of the created option
+    /// @return newQToken address of the created QToken
+    /// @return newCollateralTokenId id of the created CollateralToken
     function createOption(
         address _underlyingAsset,
         address _strikeAsset,
@@ -53,13 +64,13 @@ contract OptionsFactory {
         uint256 _strikePrice,
         uint256 _expiryTime,
         bool _isCall
-    ) external returns (address) {
+    ) external returns (address newQToken, uint256 newCollateralTokenId) {
         require(
             _expiryTime > block.timestamp,
             "OptionsFactory: given expiry time is in the past"
         );
-        bytes32 optionHash =
-            _optionHash(
+        bytes32 qTokenHash =
+            _qTokenHash(
                 _underlyingAsset,
                 _strikeAsset,
                 _oracle,
@@ -68,7 +79,7 @@ contract OptionsFactory {
                 _isCall
             );
         require(
-            _hashToAddress[optionHash] == address(0),
+            _qTokenHashToAddress[qTokenHash] == address(0),
             "OptionsFactory: option already creted"
         );
         require(
@@ -89,23 +100,27 @@ contract OptionsFactory {
                 )
             );
 
-        address newOption = Create2.deploy(0, _SALT, bytecode);
+        newQToken = Create2.deploy(0, _SALT, bytecode);
 
-        _hashToAddress[optionHash] = newOption;
-        options.push(newOption);
+        _qTokenHashToAddress[qTokenHash] = newQToken;
+        qTokens.push(newQToken);
+
+        newCollateralTokenId = _collateralToken.createCollateralToken(
+            newQToken,
+            0
+        );
 
         emit OptionCreated(
-            newOption,
+            newQToken,
             msg.sender,
             _underlyingAsset,
             _strikeAsset,
             _oracle,
             _strikePrice,
             _expiryTime,
+            newCollateralTokenId,
             _isCall
         );
-
-        return newOption;
     }
 
     /// @notice get the address at which a new QToken with the given parameters would be deployed
@@ -114,7 +129,7 @@ contract OptionsFactory {
     /// @param _underlyingAsset asset that the option references
     /// @param _strikeAsset asset that the strike is denominated in
     /// @param _oracle price oracle for the option underlying
-    /// @param _strikePrice strike price with 18 decimals
+    /// @param _strikePrice strike price with as many decimals in the strike asset
     /// @param _expiryTime expiration timestamp as a unix timestamp
     /// @param _isCall true if it's a call option, false if it's a put option
     /// @return the address where a QToken would be deployed
@@ -150,7 +165,7 @@ contract OptionsFactory {
     /// @param _underlyingAsset asset that the option references
     /// @param _strikeAsset asset that the strike is denominated in
     /// @param _oracle price oracle for the option underlying
-    /// @param _strikePrice strike price with 18 decimals
+    /// @param _strikePrice strike price with as many decimals in the strike asset
     /// @param _expiryTime expiration timestamp as a unix timestamp
     /// @param _isCall true if it's a call option, false if it's a put option
     /// @return address of the requested option
@@ -162,8 +177,8 @@ contract OptionsFactory {
         uint256 _expiryTime,
         bool _isCall
     ) external view returns (address) {
-        bytes32 optionHash =
-            _optionHash(
+        bytes32 qTokenHash =
+            _qTokenHash(
                 _underlyingAsset,
                 _strikeAsset,
                 _oracle,
@@ -172,24 +187,24 @@ contract OptionsFactory {
                 _isCall
             );
 
-        return _hashToAddress[optionHash];
+        return _qTokenHashToAddress[qTokenHash];
     }
 
     /// @notice get the total number of options created by the factory
     /// @return length of the options array
     function getOptionsLength() external view returns (uint256) {
-        return options.length;
+        return qTokens.length;
     }
 
     /// @notice Returns a unique option hash based on its parameters
     /// @param _underlyingAsset asset that the option references
     /// @param _strikeAsset asset that the strike is denominated in
     /// @param _oracle price oracle for the option underlying
-    /// @param _strikePrice strike price with 18 decimals
+    /// @param _strikePrice strike price with as many decimals in the strike asset
     /// @param _expiryTime expiration timestamp as a unix timestamp
     /// @param _isCall true if it's a call option, false if it's a put option
     /// @return 32-bytes hash unique to an option
-    function _optionHash(
+    function _qTokenHash(
         address _underlyingAsset,
         address _strikeAsset,
         address _oracle,
--- contracts/protocol/options/QToken.sol
+++ contracts/protocol/options/QToken.sol
@@ -8,47 +8,35 @@ import "@quant-finance/solidity-datetime/contracts/DateTime.sol";
 import "../QuantConfig.sol";
 import "../pricing/PriceRegistry.sol";
 
+/// @title Token that represents a user's long position
+/// @author Quant Finance
+/// @notice Can be used by owners to exercise their options
+/// @dev Every option long position is an ERC20 token: https://eips.ethereum.org/EIPS/eip-20
 contract QToken is ERC20 {
     using SafeMath for uint256;
 
-    /**
-     * @dev Address of system config.
-     */
+    /// @dev Address of system config.
     QuantConfig public quantConfig;
 
-    /**
-     * @dev Address of the underlying asset. WETH for ethereum options.
-     */
+    /// @dev Address of the underlying asset. WETH for ethereum options.
     address public underlyingAsset;
 
-    /**
-     * @dev Address of the strike asset. Quant Web options always use USDC.
-     */
+    /// @dev Address of the strike asset. Quant Web options always use USDC.
     address public strikeAsset;
 
-    /**
-     * @dev Address of the oracle to be used with this option
-     */
+    /// @dev Address of the oracle to be used with this option
     address public oracle;
 
-    /**
-     * @dev The strike price for the token with the strike asset precision.
-     */
+    /// @dev The strike price for the token with the strike asset precision.
     uint256 public strikePrice;
 
-    /**
-     * @dev UNIX time for the expiry of the option
-     */
+    /// @dev UNIX time for the expiry of the option
     uint256 public expiryTime;
 
-    /**
-     * @dev True if the option is a CALL. False if the option is a PUT.
-     */
+    /// @dev True if the option is a CALL. False if the option is a PUT.
     bool public isCall;
 
-    /**
-     * @dev Current pricing status of option. Only SETTLED options can be exercised
-     */
+    /// @dev Current pricing status of option. Only SETTLED options can be exercised
     enum PriceStatus {ACTIVE, AWAITING_SETTLEMENT_PRICE, SETTLED}
 
     uint256 private constant _STRIKE_PRICE_SCALE = 1e18;
@@ -59,7 +47,7 @@ contract QToken is ERC20 {
     /// @param _underlyingAsset asset that the option references
     /// @param _strikeAsset asset that the strike is denominated in
     /// @param _oracle price oracle for the underlying
-    /// @param _strikePrice strike price with 18 decimals
+    /// @param _strikePrice strike price with as many decimals in the strike asset
     /// @param _expiryTime expiration timestamp as a unix timestamp
     /// @param _isCall true if it's a call option, false if it's a put option
     constructor(
@@ -97,12 +85,9 @@ contract QToken is ERC20 {
         isCall = _isCall;
     }
 
-    /**
-     * @notice mint option token for an account
-     * @dev Controller only method where access control is taken care of by _beforeTokenTransfer hook
-     * @param account account to mint token to
-     * @param amount amount to mint
-     */
+    /// @notice mint option token for an account
+    /// @param account account to mint token to
+    /// @param amount amount to mint
     function mint(address account, uint256 amount) external {
         require(
             quantConfig.hasRole(
@@ -114,12 +99,9 @@ contract QToken is ERC20 {
         _mint(account, amount);
     }
 
-    /**
-     * @notice burn option token from an account.
-     * @dev Controller only method where access control is taken care of by _beforeTokenTransfer hook
-     * @param account account to burn token from
-     * @param amount amount to burn
-     */
+    /// @notice burn option token from an account.
+    /// @param account account to burn token from
+    /// @param amount amount to burn
     function burn(address account, uint256 amount) external {
         require(
             quantConfig.hasRole(
@@ -134,7 +116,7 @@ contract QToken is ERC20 {
     /// @notice generates the name for an option
     /// @param _underlyingAsset asset that the option references
     /// @param _strikeAsset asset that the strike is denominated in
-    /// @param _strikePrice strike price with 18 decimals
+    /// @param _strikePrice strike price with as many decimals in the strike asset
     /// @param _expiryTime expiration timestamp as a unix timestamp
     /// @param _isCall true if it's a call option, false if it's a put option
     /// @return tokenName name string for the QToken
@@ -184,7 +166,7 @@ contract QToken is ERC20 {
     /// @notice generates the symbol for an option
     /// @param _underlyingAsset asset that the option references
     /// @param _strikeAsset asset that the strike is denominated in
-    /// @param _strikePrice strike price with 18 decimals
+    /// @param _strikePrice strike price with as many decimals in the strike asset
     /// @param _expiryTime expiration timestamp as a unix timestamp
     /// @param _isCall true if it's a call option, false if it's a put option
     /// @return tokenSymbol symbol string for the QToken
@@ -274,18 +256,18 @@ contract QToken is ERC20 {
     /// @dev get the representation of a number using 2 characters, adding a leading 0 if it's one digit,
     /// and two trailing digits if it's a 3 digit number
     /// @return 2 characters that correspond to a number
-    function _uintToChars(uint256 number)
+    function _uintToChars(uint256 _number)
         internal
         pure
         returns (string memory)
     {
-        if (number > 99) {
-            number %= 100;
+        if (_number > 99) {
+            _number %= 100;
         }
 
-        string memory str = Strings.toString(number);
+        string memory str = Strings.toString(_number);
 
-        if (number < 10) {
+        if (_number < 10) {
             return string(abi.encodePacked("0", str));
         }
 
