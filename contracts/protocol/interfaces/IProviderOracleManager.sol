12b83a3898261b4c6e8042a0eff7ef7468f3fb53
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-04-06 18:25:28 -0300

    Run linting

--- contracts/protocol/interfaces/IProviderOracleManager.sol
+++ contracts/protocol/interfaces/IProviderOracleManager.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.7.0;
 interface IProviderOracleManager {
     function addAssetOracle(address _asset, address _oracle) external;
 
-    function getAssetOracle(address _asset) external view returns (address);
-
     function setExpiryPriceInRegistry(
         address _asset,
         uint256 _expiryTimestamp,
         bytes memory _calldata
     ) external;
+
+    function getAssetOracle(address _asset) external view returns (address);
 
     function getAssetsLength() external view returns (uint256);
 
