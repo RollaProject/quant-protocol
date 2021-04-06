a91a6c6cc7b4dbcdd43b21d8c4708df604dd9332
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-04-06 18:25:28 -0300

    Run linting

--- contracts/protocol/interfaces/IOracleRegistry.sol
+++ contracts/protocol/interfaces/IOracleRegistry.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.7.0;
 interface IOracleRegistry {
     function addOracle(address _oracle) external returns (uint256);
 
+    function deactivateOracle(address _oracle) external returns (bool);
+
+    function activateOracle(address _oracle) external returns (bool);
+
     function isOracleRegistered(address _oracle) external view returns (bool);
 
     function isOracleActive(address _oracle) external view returns (bool);
 
     function getOracleId(address _oracle) external view returns (uint256);
-
-    function deactivateOracle(address _oracle) external returns (bool);
-
-    function activateOracle(address _oracle) external returns (bool);
 
     function getOraclesLength() external view returns (uint256);
 }
