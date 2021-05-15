299f75128b7a1076dac02d47ca708105c7cc8d0f
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-05-15 19:20:47 -0300

    refactor(interfaces): update the QToken contract and interface

--- contracts/interfaces/IQToken.sol
+++ contracts/interfaces/IQToken.sol
@@ -2,14 +2,58 @@
 pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IQuantConfig.sol";
 
 /// @dev Current pricing status of option. Only SETTLED options can be exercised
 enum PriceStatus {ACTIVE, AWAITING_SETTLEMENT_PRICE, SETTLED}
 
+/// @title Token that represents a user's long position
+/// @author Quant Finance
+/// @notice Can be used by owners to exercise their options
+/// @dev Every option long position is an ERC20 token: https://eips.ethereum.org/EIPS/eip-20
 interface IQToken is IERC20 {
+    /// @notice event emitted when QTokens are minted
+    /// @param account account the QToken was minted to
+    /// @param amount the amount of QToken minted
+    event QTokenMinted(address indexed account, uint256 amount);
+
+    /// @notice event emitted when QTokens are burned
+    /// @param account account the QToken was burned from
+    /// @param amount the amount of QToken burned
+    event QTokenBurned(address indexed account, uint256 amount);
+
+    /// @notice mint option token for an account
+    /// @param account account to mint token to
+    /// @param amount amount to mint
     function mint(address account, uint256 amount) external;
 
+    /// @notice burn option token from an account.
+    /// @param account account to burn token from
+    /// @param amount amount to burn
     function burn(address account, uint256 amount) external;
 
+    /// @dev Address of system config.
+    function quantConfig() external view returns (IQuantConfig);
+
+    /// @dev Address of the underlying asset. WETH for ethereum options.
+    function underlyingAsset() external view returns (address);
+
+    /// @dev Address of the strike asset. Quant Web options always use USDC.
+    function strikeAsset() external view returns (address);
+
+    /// @dev Address of the oracle to be used with this option
+    function oracle() external view returns (address);
+
+    /// @dev The strike price for the token with the strike asset precision.
+    function strikePrice() external view returns (uint256);
+
+    /// @dev UNIX time for the expiry of the option
+    function expiryTime() external view returns (uint256);
+
+    /// @dev True if the option is a CALL. False if the option is a PUT.
+    function isCall() external view returns (bool);
+
+    /// @notice Get the price status of the option.
+    /// @return the price status of the option. option is either active, awaiting settlement price or settled
     function getOptionPriceStatus() external view returns (PriceStatus);
 }
