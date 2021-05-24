15b35b19c2fb7c225b6c1b83c1a1d03a1cb7a7fb
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-05-24 02:11:18 -0300

    feat(QToken): add the permit functionality (signed approvals)

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,4 +1,4 @@
-import { BigNumber, Signer } from "ethers";
+import { BigNumber, Contract, Signer } from "ethers";
 import { ethers, upgrades, waffle } from "hardhat";
 import AssetsRegistryJSON from "../artifacts/contracts/options/AssetsRegistry.sol/AssetsRegistry.json";
 import CollateralTokenJSON from "../artifacts/contracts/options/CollateralToken.sol/CollateralToken.json";
@@ -17,8 +17,17 @@ import { CollateralToken } from "../typechain/CollateralToken";
 import { MockERC20 } from "../typechain/MockERC20";
 import { QToken } from "../typechain/QToken";
 import { QuantConfig } from "../typechain/QuantConfig";
+import { provider } from "./setup";
 
 const { deployContract } = waffle;
+
+const { keccak256, defaultAbiCoder, toUtf8Bytes, solidityPack } = ethers.utils;
+
+const PERMIT_TYPEHASH = keccak256(
+  toUtf8Bytes(
+    "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+  )
+);
 
 const mockERC20 = async (
   deployer: Signer,
@@ -171,6 +180,62 @@ const deployConfigTimelockController = async (
   return configTimelockController;
 };
 
+const getDomainSeparator = (name: string, tokenAddress: string): string => {
+  return keccak256(
+    defaultAbiCoder.encode(
+      ["bytes32", "bytes32", "bytes32", "uint256", "address"],
+      [
+        keccak256(
+          toUtf8Bytes(
+            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+          )
+        ),
+        keccak256(toUtf8Bytes(name)),
+        keccak256(toUtf8Bytes("1")),
+        provider.network.chainId,
+        tokenAddress,
+      ]
+    )
+  );
+};
+
+const getApprovalDigest = async (
+  token: Contract,
+  approve: {
+    owner: string;
+    spender: string;
+    value: BigNumber;
+  },
+  nonce: BigNumber,
+  deadline: BigNumber
+): Promise<string> => {
+  const name = await token.name();
+  const DOMAIN_SEPARATOR = getDomainSeparator(name, token.address);
+  return keccak256(
+    solidityPack(
+      ["bytes1", "bytes1", "bytes32", "bytes32"],
+      [
+        "0x19",
+        "0x01",
+        DOMAIN_SEPARATOR,
+        keccak256(
+          defaultAbiCoder.encode(
+            ["bytes32", "address", "address", "uint256", "uint256", "uint256"],
+            [
+              PERMIT_TYPEHASH,
+              approve.owner,
+              approve.spender,
+              approve.value,
+              nonce,
+              deadline,
+            ]
+          )
+        ),
+      ]
+    )
+  );
+};
+
 export {
   deployCollateralToken,
   deployOptionsFactory,
@@ -180,4 +245,5 @@ export {
   deployOracleRegistry,
   deployConfigTimelockController,
   mockERC20,
+  getApprovalDigest,
 };
