5e58581cd978734db11b4660006ad9fea0cfc153
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-05-26 00:10:14 -0300

    test: mint options through meta transactions

---
+++ test/eip712Types.ts
@@ -0,0 +1,33 @@
+export const domainType = [
+  {
+    name: "name",
+    type: "string",
+  },
+  {
+    name: "version",
+    type: "string",
+  },
+  {
+    name: "chainId",
+    type: "uint256",
+  },
+  {
+    name: "verifyingContract",
+    type: "address",
+  },
+];
+
+export const metaTransactionType = [
+  {
+    name: "nonce",
+    type: "uint256",
+  },
+  {
+    name: "from",
+    type: "address",
+  },
+  {
+    name: "functionSignature",
+    type: "bytes",
+  },
+];
