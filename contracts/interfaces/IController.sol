737597416cc1ea1c6ae094d7b6d0248f798004c4
optionseskimo <optionseskimo@protonmail.com>
2021-05-26 21:36:07 -0300

    feat: make the Controller upgradeable

--- contracts/interfaces/IController.sol
+++ contracts/interfaces/IController.sol
@@ -63,6 +63,12 @@ interface IController {
     function neutralizePosition(uint256 _collateralTokenId, uint256 _amount)
         external;
 
+    function initialize(
+        string memory,
+        string memory,
+        address
+    ) external;
+
     function getCollateralRequirement(
         address _qTokenToMint,
         address _qTokenForCollateral,
