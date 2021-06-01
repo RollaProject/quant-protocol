c5bb7641721e9cefdb6cc378660b68a4fa5482fd
optionseskimo <optionseskimo@protonmail.com>
2021-05-31 23:33:16 -0300

    feat: add a mock of a Chainlink oracle

---
+++ contracts/test/MockChainlinkAggregatorProxy.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+contract MockChainlinkAggregatorProxy {
+    int256 private _defaultLatestAnswer;
+
+    constructor(int256 defaultLatestAnswer_) {
+        _defaultLatestAnswer = defaultLatestAnswer_;
+    }
+
+    function latestAnswer() external view returns (int256) {
+        return _defaultLatestAnswer;
+    }
+}
