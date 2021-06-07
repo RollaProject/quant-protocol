a6d4d62e626f487415178862e6d2bd214ceb5578
Nurit Dor <nurit@certora.com>
2021-06-07 21:23:06 +0300

    add quantcalculator to list of contracts

--- spec/scripts/runController.sh
+++ spec/scripts/runController.sh
@@ -1,6 +1,8 @@
 certoraRun spec/harness/ControllerHarness.sol spec/harness/DummyERC20A.sol \
 spec/harness/DummyERC20B.sol spec/harness/QTokenA.sol spec/harness/QTokenB.sol \
 	contracts/options/OptionsFactory.sol spec/harness/CollateralTokenHarness.sol \
+	contracts/QuantCalculator.sol \
+	--link ControllerHarness:quantCalculator=QuantCalculator \
     --verify ControllerHarness:spec/controller.spec --settings -ignoreViewFunctions,-postProcessCounterExamples=true \
 	--solc solc7.6 \
 	--rule $1 \
