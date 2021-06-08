a14e1c92d0cf3c5c9cd0cf6eda206e2da1b05083
optionseskimo <optionseskimo@protonmail.com>
2021-06-08 02:17:01 -0300

    feat: add ERC1155 with internal _operatorApprovals so they can be set by meta-txs

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
   testCommand: "hardhat test",
   compileCommand: "hardhat compile",
-  skipFiles: ["external/", "test/"],
+  skipFiles: ["external/", "test/", "libraries/external"],
 };
