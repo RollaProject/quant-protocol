6b36b7897a2ff4dcf5e22c22b2552d0b652197b1
Gadi Rechlis <gr@013.net>
2021-06-10 18:22:41 +0300

    run script

--- spec/scripts/runController.sh
+++ spec/scripts/runController.sh
@@ -2,8 +2,12 @@ certoraRun spec/harness/ControllerHarness.sol spec/harness/DummyERC20A.sol \
 spec/harness/DummyERC20B.sol spec/harness/QTokenA.sol spec/harness/QTokenB.sol \
 	contracts/options/OptionsFactory.sol spec/harness/CollateralTokenHarness.sol \
 	contracts/QuantCalculator.sol \
-	--link ControllerHarness:quantCalculator=QuantCalculator \
-    --verify ControllerHarness:spec/controller.spec --settings -ignoreViewFunctions,-postProcessCounterExamples=true \
+	--verify ControllerHarness:spec/controller.spec --settings -ignoreViewFunctions,-postProcessCounterExamples=true \
 	--solc solc7.6 \
 	--rule $1 \
 	--staging --msg "Controller : $1 - $2 "
+
+
+
+#	--link ControllerHarness:quantCalculator=QuantCalculator \
+    
