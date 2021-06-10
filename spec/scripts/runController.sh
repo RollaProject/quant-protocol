c52464fcccaa479aff949ab07f91460df132b59f
Shelly Grossman <shellygr151@gmail.com>
2021-06-10 16:37:35 +0300

    fixing overflow of memory

--- spec/scripts/runController.sh
+++ spec/scripts/runController.sh
@@ -1,7 +1,7 @@
 certoraRun spec/harness/ControllerHarness.sol spec/harness/DummyERC20A.sol \
 spec/harness/DummyERC20B.sol spec/harness/QTokenA.sol spec/harness/QTokenB.sol contracts/QuantCalculator.sol \
 	contracts/options/OptionsFactory.sol spec/harness/CollateralTokenHarness.sol \
-	--verify ControllerHarness:spec/controller.spec --settings -ignoreViewFunctions,-postProcessCounterExamples=true \
+	--verify ControllerHarness:spec/controller.spec --settings -optimisticReturnsize=true,-ignoreViewFunctions,-postProcessCounterExamples=true \
 	--solc solc7.6 \
 	--rule $1 \
 	--staging --msg "Controller : $1 "
