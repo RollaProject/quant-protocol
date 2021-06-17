07287477312fec12fc8f0e460b863f282b7ebf87
quant-spiz <78666323+quant-spiz@users.noreply.github.com>
2021-06-17 22:52:07 +0100

    Add referral registry

---
+++ contracts/periphery/ReferralRegistry.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "hardhat/console.sol";
+
+/// @title A registry for managing users and their referrers
+contract ReferralRegistry {
+    bytes32 public constant DEFAULT_CODE = "0";
+
+    uint256 public maxCodesPerUser;
+
+    address public immutable defaultReferrer;
+
+    /// @notice mapping to store codes and their owners
+    mapping(bytes32 => address) public codeOwner;
+
+    /// @notice mapping to store users and their referrers
+    mapping(address => address) public userReferrer;
+
+    /// @notice mapping to store users and their codes
+    mapping(address => bytes32[]) public userCodes;
+
+    event NewUserRegistration(
+        address indexed referred,
+        address indexed referrer,
+        bytes32 code
+    );
+    event CreatedReferralCode(address indexed user, bytes32 code);
+
+    /// @param _defaultReferrer Default referrer address
+    /// @param _maxCodesPerUser Maximum number of codes a single user can claim
+    constructor(address _defaultReferrer, uint256 _maxCodesPerUser) {
+        defaultReferrer = _defaultReferrer;
+        maxCodesPerUser = _maxCodesPerUser;
+        _createReferralCode(_defaultReferrer, DEFAULT_CODE);
+    }
+
+    /// @notice Allows a user to claim a custom referral code
+    /// @param code The code for the user to claim
+    function claimReferralCode(bytes32 code) external {
+        require(!isCodeUsed(code), "ReferralRegistry: code already exists");
+        require(
+            userCodes[msg.sender].length < maxCodesPerUser,
+            "ReferralRegistry: user has claimed all their codes"
+        );
+        _createReferralCode(msg.sender, code);
+    }
+
+    /// @notice Add referral code to registry
+    /// @param user The user which is claiming a code
+    /// @param code The code for the user to claim
+    function _createReferralCode(address user, bytes32 code) internal {
+        codeOwner[code] = user;
+        userCodes[msg.sender].push(code);
+        emit CreatedReferralCode(user, code);
+    }
+
+    /// @notice Register to Quant using a referral code
+    /// @param code The code for the user to sign up with
+    function registerUserByReferralCode(bytes32 code) external {
+        address referrer = codeOwner[code];
+        if (referrer == address(0)) {
+            referrer = defaultReferrer;
+        }
+        _registerUser(referrer, code);
+    }
+
+    /// @notice Register to Quant using a referrer's address
+    /// @param referrer Address of the referrer
+    function registerUserByReferrer(address referrer) external {
+        _registerUser(referrer, "");
+    }
+
+    /// @notice Register a user in the system
+    /// @param referrer Address of the referrer
+    /// @param code Referral code used. Default code if no code used
+    function _registerUser(address referrer, bytes32 code) internal {
+        require(
+            userReferrer[msg.sender] == address(0),
+            "ReferralRegistry: cannot register twice"
+        );
+        require(referrer != msg.sender, "ReferralRegistry: cannot refer self");
+        userReferrer[msg.sender] = referrer;
+        emit NewUserRegistration(msg.sender, referrer, code);
+    }
+
+    /// @notice Check if a code has been claimed by another user
+    /// @param code the code to check
+    /// @return true if the code has been claimed otherwise false
+    function isCodeUsed(bytes32 code) public view returns (bool) {
+        return codeOwner[code] != address(0);
+    }
+
+    /// @notice Check who a user is referred by
+    /// @param user the user to get the referrer of
+    function getReferrer(address user)
+        external
+        view
+        returns (address referrer)
+    {
+        return
+            userReferrer[user] != address(0)
+                ? userReferrer[user]
+                : defaultReferrer;
+    }
+}
---
+++ test/ReferralRegistry.test.ts
@@ -0,0 +1,123 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { expect } from "chai";
+import { formatBytes32String } from "ethers/lib/utils";
+import { ethers, waffle } from "hardhat";
+import ReferralRegistryJSON from "../artifacts/contracts/periphery/ReferralRegistry.sol/ReferralRegistry.json";
+import { ReferralRegistry } from "../typechain";
+
+const { deployContract } = waffle;
+
+describe("Referral Registry", () => {
+  let signers: SignerWithAddress[];
+  let deployer: SignerWithAddress;
+  let signer: SignerWithAddress;
+  let signerTwo: SignerWithAddress;
+  let defaultReferrer: SignerWithAddress;
+  let referralRegistry: ReferralRegistry;
+
+  const dummyCode = formatBytes32String("test");
+  const dummyCode1 = formatBytes32String("test1");
+  const dummyCode2 = formatBytes32String("test2");
+  const dummyCode3 = formatBytes32String("test3");
+  const DEFAULT_CODE = formatBytes32String("0");
+
+  before(async function () {
+    [deployer, signer, signerTwo, defaultReferrer] = await ethers.getSigners();
+  });
+
+  describe("ReferralRegistry", () => {
+    beforeEach(async function () {
+      referralRegistry = (await deployContract(deployer, ReferralRegistryJSON, [
+        await defaultReferrer.getAddress(),
+        3,
+      ])) as ReferralRegistry;
+    });
+
+    it("should create a referral and emit an event for its creator", async () => {
+      await expect(
+        referralRegistry.connect(signer).claimReferralCode(dummyCode)
+      )
+        .to.emit(referralRegistry, "CreatedReferralCode")
+        .withArgs(await signer.getAddress(), dummyCode);
+      expect(await referralRegistry.codeOwner(dummyCode)).to.be.equal(
+        await signer.getAddress()
+      );
+    });
+
+    it("registers a user by a referer", async () => {
+      await expect(
+        referralRegistry
+          .connect(signer)
+          .registerUserByReferrer(await signerTwo.getAddress())
+      )
+        .to.emit(referralRegistry, "NewUserRegistration")
+        .withArgs(
+          await signer.getAddress(),
+          await signerTwo.getAddress(),
+          ethers.constants.HashZero
+        );
+      expect(
+        await referralRegistry.userReferrer(await signer.getAddress())
+      ).to.be.equal(await signerTwo.getAddress());
+    });
+
+    it("a user cannot claim more codes than allowed", async () => {
+      await referralRegistry.connect(signer).claimReferralCode(dummyCode);
+      await referralRegistry.connect(signer).claimReferralCode(dummyCode1);
+      await referralRegistry.connect(signer).claimReferralCode(dummyCode2);
+      await expect(
+        referralRegistry.connect(signer).claimReferralCode(dummyCode3)
+      ).to.be.revertedWith(
+        "ReferralRegistry: user has claimed all their codes"
+      );
+    });
+
+    it("cannot self refer", async () => {
+      await referralRegistry.connect(signer).claimReferralCode(dummyCode);
+      await expect(
+        referralRegistry.connect(signer).registerUserByReferralCode(dummyCode)
+      ).to.be.revertedWith("ReferralRegistry: cannot refer self");
+    });
+
+    // it("cannot register non-existing codes", async () => {
+    //   await expect(
+    //     referralRegistry.registerUserByReferralCode(formatBytes32String("lol"))
+    //   ).to.be.revertedWith("ReferralRegistry: code does not exist");
+    // });
+
+    it("cannot use same code", async () => {
+      await referralRegistry.connect(signer).claimReferralCode(dummyCode);
+      await expect(
+        referralRegistry.connect(signer).claimReferralCode(dummyCode)
+      ).to.be.revertedWith("ReferralRegistry: code already exists");
+    });
+
+    it("cannot register twice", async () => {
+      await referralRegistry.connect(signerTwo).claimReferralCode(dummyCode);
+      await referralRegistry
+        .connect(signer)
+        .registerUserByReferralCode(dummyCode);
+      await expect(
+        referralRegistry.connect(signer).registerUserByReferralCode(dummyCode)
+      ).to.be.revertedWith("ReferralRegistry: cannot register twice");
+    });
+
+    it("by default referrals point to owner", async () => {
+      expect(
+        await referralRegistry
+          .connect(signer)
+          .getReferrer(await signerTwo.getAddress())
+      ).to.be.equal(await referralRegistry.defaultReferrer());
+    });
+
+    it("refers a new user", async () => {
+      await referralRegistry.connect(signer).claimReferralCode(dummyCode);
+      await referralRegistry
+        .connect(signerTwo)
+        .registerUserByReferralCode(dummyCode);
+      expect(
+        await referralRegistry.getReferrer(await signerTwo.getAddress())
+      ).to.be.equal(await signer.getAddress());
+    });
+  });
+});
