d823d6426add126f1c51c4586ae08424b0c3b9bf
optionseskimo <optionseskimo@protonmail.com>
2021-06-19 15:14:02 -0300

    Revert "test(coverage): ignore an external, barely modified OpenZeppelin contract"
    This reverts commit 2e5c67d934f461770fac0447df5ae7a9f4c87359.

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
   testCommand: "hardhat test",
   compileCommand: "hardhat compile",
-  skipFiles: ["external/", "test/", "libraries/external", "timelock/TimelockController.sol"],
+  skipFiles: ["external/", "test/", "libraries/external"],
 };
