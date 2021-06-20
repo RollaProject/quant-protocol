d607857d8ae2cb10ddb0832b70a12951d34def4e
optionseskimo <optionseskimo@protonmail.com>
2021-06-20 00:11:20 -0300

    test: update unit tests for the ReferralRegistry

--- test/ReferralRegistry.test.ts
+++ test/ReferralRegistry.test.ts
@@ -14,10 +14,10 @@ describe("Referral Registry", () => {
   let defaultReferrer: SignerWithAddress;
   let referralRegistry: ReferralRegistry;
 
-  const dummyCode = formatBytes32String("test");
-  const dummyCode1 = formatBytes32String("test1");
-  const dummyCode2 = formatBytes32String("test2");
-  const dummyCode3 = formatBytes32String("test3");
+  const dummyCode = "test";
+  const dummyCode1 = "test1";
+  const dummyCode2 = "test2";
+  const dummyCode3 = "test3";
 
   before(async function () {
     [deployer, signer, signerTwo, defaultReferrer] = await ethers.getSigners();
@@ -36,10 +36,10 @@ describe("Referral Registry", () => {
         referralRegistry.connect(signer).claimReferralCode(dummyCode)
       )
         .to.emit(referralRegistry, "CreatedReferralCode")
-        .withArgs(await signer.getAddress(), dummyCode);
-      expect(await referralRegistry.codeOwner(dummyCode)).to.be.equal(
-        await signer.getAddress()
-      );
+        .withArgs(await signer.getAddress(), formatBytes32String(dummyCode));
+      expect(
+        await referralRegistry.codeOwner(formatBytes32String(dummyCode))
+      ).to.be.equal(await signer.getAddress());
     });
 
     it("registers a user by a referer", async () => {
@@ -73,15 +73,24 @@ describe("Referral Registry", () => {
     it("cannot self refer", async () => {
       await referralRegistry.connect(signer).claimReferralCode(dummyCode);
       await expect(
-        referralRegistry.connect(signer).registerUserByReferralCode(dummyCode)
+        referralRegistry
+          .connect(signer)
+          .registerUserByReferralCode(formatBytes32String(dummyCode))
       ).to.be.revertedWith("ReferralRegistry: cannot refer self");
     });
 
-    // it("cannot register non-existing codes", async () => {
-    //   await expect(
-    //     referralRegistry.registerUserByReferralCode(formatBytes32String("lol"))
-    //   ).to.be.revertedWith("ReferralRegistry: code does not exist");
-    // });
+    it("non-existing codes should register with the defaultReferrer", async () => {
+      const code = formatBytes32String("badcode");
+      await expect(
+        referralRegistry.connect(signerTwo).registerUserByReferralCode(code)
+      )
+        .to.emit(referralRegistry, "NewUserRegistration")
+        .withArgs(signerTwo.address, defaultReferrer.address, code);
+
+      expect(await referralRegistry.userReferrer(signerTwo.address)).to.equal(
+        defaultReferrer.address
+      );
+    });
 
     it("cannot use same code", async () => {
       await referralRegistry.connect(signer).claimReferralCode(dummyCode);
@@ -94,9 +103,11 @@ describe("Referral Registry", () => {
       await referralRegistry.connect(signerTwo).claimReferralCode(dummyCode);
       await referralRegistry
         .connect(signer)
-        .registerUserByReferralCode(dummyCode);
+        .registerUserByReferralCode(formatBytes32String(dummyCode));
       await expect(
-        referralRegistry.connect(signer).registerUserByReferralCode(dummyCode)
+        referralRegistry
+          .connect(signer)
+          .registerUserByReferralCode(formatBytes32String(dummyCode))
       ).to.be.revertedWith("ReferralRegistry: cannot register twice");
     });
 
@@ -112,7 +123,7 @@ describe("Referral Registry", () => {
       await referralRegistry.connect(signer).claimReferralCode(dummyCode);
       await referralRegistry
         .connect(signerTwo)
-        .registerUserByReferralCode(dummyCode);
+        .registerUserByReferralCode(formatBytes32String(dummyCode));
       expect(
         await referralRegistry.getReferrer(await signerTwo.getAddress())
       ).to.be.equal(await signer.getAddress());
