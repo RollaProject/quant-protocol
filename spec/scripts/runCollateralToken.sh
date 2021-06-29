1ee8b45721e052f322b5c4de73223100b43485bf
Nurit Dor <nurit@certora.com>
2021-06-29 17:12:39 +0300

    ci-mode to improve running time

--- spec/scripts/runCollateralToken.sh
+++ spec/scripts/runCollateralToken.sh
@@ -1,4 +1,4 @@
 certoraRun spec/harness/CollateralTokenHarness.sol \
 --verify CollateralTokenHarness:spec/collateralToken.spec \
---settings -assumeUnwindCond --cache CollateralToken \
+--settings -assumeUnwindCond,-ciMode=true --cache CollateralToken \
 --staging --msg "CollateralToken"
--- spec/scripts/runFundsCalculator.sh
+++ spec/scripts/runFundsCalculator.sh
@@ -1,5 +1,5 @@
 certoraRun spec/harness/FundsCalculatorWrapper.sol contracts/libraries/FundsCalculator.sol spec/harness/DummyERC20A.sol \
            spec/harness/DummyERC20B.sol spec/harness/QTokenA.sol spec/harness/QTokenB.sol \
             --verify FundsCalculatorWrapper:spec/fundsCalculator.spec \
-            --settings -assumeUnwindCond,-postProcessCounterExamples=true,-smt_nonLinearArithmetic=true\
+            --settings -assumeUnwindCond,-postProcessCounterExamples=true,-smt_nonLinearArithmetic=true,-ciMode=true \
             --staging --msg "FundsCalculator"
