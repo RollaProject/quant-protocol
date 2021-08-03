ced45f6a529a7cab869277c3963a4d96e347fdd9
optionseskimo <optionseskimo@protonmail.com>
2021-08-03 16:20:52 -0300

    chore: update license (MIT -> BUSL-1.1)

--- spec/harness/QTokenCore.sol
+++ spec/harness/QTokenCore.sol
@@ -1,13 +1,11 @@
-
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.7.0;
 pragma abicoder v2;
 
 import "../../contracts/options/QToken.sol";
 
 contract QTokenCore is QToken {
-
-	constructor(
+    constructor(
         address _quantConfig,
         address _underlyingAsset,
         address _strikeAsset,
@@ -15,14 +13,25 @@ contract QTokenCore is QToken {
         uint256 _strikePrice,
         uint256 _expiryTime,
         bool _isCall
-    )  QToken(_quantConfig, _underlyingAsset, _strikeAsset, _oracle, _strikePrice, _expiryTime, _isCall) public {}
-
+    )
+        public
+        QToken(
+            _quantConfig,
+            _underlyingAsset,
+            _strikeAsset,
+            _oracle,
+            _strikePrice,
+            _expiryTime,
+            _isCall
+        )
+    {}
 
     function _assetSymbol(address _quantConfig, address _asset)
         internal
-        view override
+        view
+        override
         returns (string memory symbol)
-    { }
+    {}
 
     function _qTokenName(
         address _quantConfig,
@@ -31,7 +40,7 @@ contract QTokenCore is QToken {
         uint256 _strikePrice,
         uint256 _expiryTime,
         bool _isCall
-    ) internal view override returns (string memory tokenName) { }
+    ) internal view override returns (string memory tokenName) {}
 
     function _qTokenSymbol(
         address _quantConfig,
@@ -40,39 +49,39 @@ contract QTokenCore is QToken {
         uint256 _strikePrice,
         uint256 _expiryTime,
         bool _isCall
-    ) internal view override returns (string memory tokenSymbol) { }
+    ) internal view override returns (string memory tokenSymbol) {}
 
     function _getOptionType(bool _isCall)
         internal
-        pure override
+        pure
+        override
         returns (string memory, string memory)
-    { }
+    {}
 
     function _displayedStrikePrice(uint256 _strikePrice)
         internal
-        pure override
+        pure
+        override
         returns (string memory)
-    { }
-
+    {}
 
     function _uintToChars(uint256 _number)
         internal
-        pure override
+        pure
+        override
         returns (string memory)
-    { }
+    {}
 
     function _slice(
         string memory _s,
         uint256 _start,
         uint256 _end
-    ) internal pure override returns (string memory) { }
-
+    ) internal pure override returns (string memory) {}
 
     function _getMonth(uint256 _month)
         internal
-        pure override
+        pure
+        override
         returns (string memory, string memory)
-    { }
-
-
- }
+    {}
+}
