aea69733761f77cfa09c98ddb352b2d376de1795
optionseskimo <78507171+optionseskimo@users.noreply.github.com>
2021-11-12 21:27:57 -0300

    build: add a Mumbai network to Hardhat's config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,6 +20,15 @@ const config: HardhatUserConfig = {
           "word soft garden squirrel this lift object foot someone boost certain provide",
       },
     },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+      chainId: 80001,
+      accounts: {
+        mnemonic:
+          process.env.MNEMONIC ||
+          "word soft garden squirrel this lift object foot someone boost certain provide",
+      },
+    },
   },
   solidity: {
     version: "0.7.6",
