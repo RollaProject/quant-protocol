c4b71a2f866c03b645691b3ec60fa8b05c80d68d
quantizations <78666323+quantizations@users.noreply.github.com>
2022-02-12 13:29:29 +0700

    Add missing events from files

---
+++ contracts/interfaces/IChainlinkFixedTimeOracleManager.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.7.0;
+
+import "./IChainlinkOracleManager.sol";
+
+interface IChainlinkFixedTimeOracleManager is IChainlinkOracleManager {
+    event FixedTimeUpdate(
+        uint256 fixedTime,
+        bool isValidTime
+    );
+
+    function setFixedTimeUpdate(uint256 fixedTime, bool isValidTime) external;
+
+    /// @notice fixed time => is allowed
+    function chainlinkFixedTimeUpdates(uint256) external view returns (bool);
+}
