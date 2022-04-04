f98ce7964758b7a60dcc4b6eb4295a8fff1e56a5
0xca11 <0xca11@protonmail.com>
2022-04-04 18:15:58 +0700

    fix(operate-proxy): check that the callee is a contract

--- contracts/utils/OperateProxy.sol
+++ contracts/utils/OperateProxy.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.13;
 
+import "@openzeppelin/contracts/utils/Address.sol";
 import "../interfaces/IOperateProxy.sol";
 
 /// @title Contract to be used by the Controller to make unprivileged external calls
 /// @author Rolla
 contract OperateProxy is IOperateProxy {
+    using Address for address;
+
     /// @inheritdoc IOperateProxy
     function callFunction(address callee, bytes memory data) external override {
-        require(
-            callee != address(0),
-            "OperateProxy: cannot make function calls to the zero address"
-        );
+        require(callee.isContract(), "OperateProxy: callee is not a contract");
 
         (bool success, bytes memory returnData) = address(callee).call(data);
         require(success, "OperateProxy: low-level call failed");
