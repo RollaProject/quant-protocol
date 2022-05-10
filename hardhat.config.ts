1bf9465f86b9967da4e347a621e8a7291e999eee
0xca11 <0xca11@protonmail.com>
2022-05-11 01:34:57 +0700

    build: update Hardhat cache that was conflicting with Foundry's

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -107,6 +107,8 @@ const config: HardhatUserConfig = {
       `https://api.bscscan.com/api?module=proxy&action=eth_gasPrice&apikey=${process.env.BSCSCAN_API_KEY}`,
   },
 
+  paths: { cache: "hh-cache" },
+
   tenderly: {
     project: process.env.ETH_NETWORK
       ? `rolla-v1-${process.env.ETH_NETWORK}`
