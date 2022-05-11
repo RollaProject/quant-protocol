59a09b41c2d4b23b0febaff5d6b6592e53e484c7
0xca11 <0xca11@protonmail.com>
2022-05-11 14:32:21 +0700

    build: update Certora harnesses

--- spec/harness/QTokenB.sol
+++ spec/harness/QTokenB.sol
@@ -3,27 +3,4 @@ pragma solidity 0.8.13;
 
 import "./QTokenCore.sol";
 
-contract QTokenB is QTokenCore {
-    constructor(
-        address _underlyingAsset,
-        address _strikeAsset,
-        address _priceRegistry,
-        address _assetsRegistry,
-        address _oracle,
-        uint88 _expiryTime,
-        bool _isCall,
-        uint256 _strikePrice
-    )
-        public
-        QTokenCore(
-            _underlyingAsset,
-            _strikeAsset,
-            _priceRegistry,
-            _assetsRegistry,
-            _oracle,
-            _expiryTime,
-            _isCall,
-            _strikePrice
-        )
-    {}
-}
+contract QTokenB is QTokenCore {}
