e53ca2cb4f22b371808bebd311b755982e6a535a
0xca11 <0xca11@protonmail.com>
2022-06-13 14:50:52 -0300

    Revert "build: update Certora FundsCalculator spec run script"
    This reverts commit db29ea90de76438077d75dd28dc96e9dfc2064f7.

--- spec/scripts/runFundsCalculator.sh
+++ spec/scripts/runFundsCalculator.sh
@@ -2,4 +2,5 @@ certoraRun spec/harness/FundsCalculatorWrapper.sol contracts/libraries/FundsCalc
            spec/harness/DummyERC20B.sol spec/harness/QTokenA.sol spec/harness/QTokenB.sol \
             --verify FundsCalculatorWrapper:spec/fundsCalculator.spec \
             --settings -assumeUnwindCond,-postProcessCounterExamples=true,-smt_nonLinearArithmetic=true,-ciMode=true \
+            --staging yoav/forThomas \
             --msg "FundsCalculator"
